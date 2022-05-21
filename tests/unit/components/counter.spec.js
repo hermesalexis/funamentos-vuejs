01aa1a97728829cc40981d3e4d6236f1b2007e08
hermesalexis <hermesagm@gmail.com>
2022-05-21 08:25:44 -0500

    test to evaluate h2 label

--- tests/unit/components/counter.spec.js
+++ tests/unit/components/counter.spec.js
@@ -3,11 +3,22 @@ import Counter from "@/components/Counter"
 
 
 describe("Counter Component", () => {
-
+/*
   test("should match with snapshot", () => {
 
     const wrapper = shallowMount( Counter )
 
     expect( wrapper.html() ).toMatchSnapshot()
+  }) */
+
+  test("h2 label should has default value 'Custom Title'", () => {
+    const wrapper = shallowMount( Counter ) // test subject
+
+    expect( wrapper.find("h2").exists() ).toBeTruthy()
+
+    const h2Value = wrapper.find('h2').text()
+
+    expect( h2Value ).toBe("Custom Title")
+
   })
 })
